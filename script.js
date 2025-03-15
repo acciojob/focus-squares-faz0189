02fe9b13eef7f9bfc0c26a41b154d12b75f8d019
faz0189 <128984518+faz0189@users.noreply.github.com>
2025-03-16 02:01:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let squares = document.querySelectorAll(".sqaure")
 
 function switchcolors(e){
 	squares.forEach(square=>{
-		square.style.background = (sqaure == event.target) ? '#E6E6FA':'6F4E37';
+		square.style.background = (sqaure === event.target) ? '#E6E6FA':'6F4E37';
 	});
 	
 }
